9325a0ecdcca80fb557deef0b9d4245b8227731f
Mario Juez-Gil <mariojg@ubu.es>
2018-11-23 14:00:46 +0100

    divided into master and worker images

---
+++ master/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Author: Mario Juez-Gil <mariojg@ubu.es>
+# Spark master node init script.
+#
+# source: https://github.com/big-data-europe/docker-spark/blob/master/master/master.sh
+
+set -eu pipefail
+
+export SPARK_MASTER_HOST=$(hostname)
+export SPARK_HOME=/spark
+
+# configuring and loading spark environment.
+. "/spark/sbin/spark-config.sh"
+. "/spark/bin/load-spark-env.sh"
+
+mkdir -p $SPARK_MASTER_LOGS
+
+ln -sf /dev/stdout $SPARK_MASTER_LOGS/spark-master.out
+
+/spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
+--ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOGS/spark-master.out
---
+++ worker/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Author: Mario Juez-Gil <mariojg@ubu.es>
+# Spark master node init script.
+#
+# source: https://github.com/big-data-europe/docker-spark/blob/master/master/master.sh
+
+set -eu pipefail
+
+export SPARK_HOME=/spark
+
+# configuring and loading spark environment.
+. "/spark/sbin/spark-config.sh"
+. "/spark/bin/load-spark-env.sh"
+
+mkdir -p $SPARK_WORKER_LOGS
+
+ln -sf /dev/stdout $SPARK_WORKER_LOGS/spark-master.out
+
+/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
+--webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOGS/spark-worker.out
