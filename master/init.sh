f038a2a2f61d4c53727df3f60f542016bce58e58
Mario Juez Gil <mariojg@ubu.es>
2018-11-28 17:30:29 +0100

    fixed dockerfile

--- master/init.sh
+++ master/init.sh
@@ -15,7 +15,7 @@ export SPARK_HOME=/spark
 
 mkdir -p $SPARK_MASTER_LOGS
 
-ln -sf /dev/stdout $SPARK_MASTER_LOGS/spark-master.out
+ln -sf /dev/stdout $SPARK_MASTER_LOGS/spark-master.outi
 
 /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
 --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOGS/spark-master.out
