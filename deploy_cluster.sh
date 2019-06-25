d58339ac0d5e17b88a8140c13ac2155fad82d441
Mario Juez <mariojg@ubu.es>
2019-06-25 12:36:29 +0200

    master node deployment

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+HOST_DATA_DIR=$1
+MASTER_CPUS=$2
+
 docker build ./base -t spark-base:2.4.3
 docker build ./master -t spark-master:2.4.3
 docker build ./worker -t spark-worker:2.4.3
+
+docker run -dP --mount type=bind,source="$HOST_DATA_DIR",target=/mnt/data --name spark-master -h spark-master --cpus $MASTER_CPUS spark-master:2.4.3
