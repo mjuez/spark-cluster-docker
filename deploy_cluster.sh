3f1cfc4c519ce67ded8db4ccaebec65ac767297e
Mario Juez Gil <mariojg@ubu.es>
2019-06-25 11:51:45 +0200

    modified base tag

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build ./base -t admirable/spark-base:2.4.3
-docker build ./master -t admirable/spark-master:2.4.3
-docker build ./worker -t admirable/spark-worker:2.4.3
+docker build ./base -t spark-base:2.4.3
+docker build ./master -t spark-master:2.4.3
+docker build ./worker -t spark-worker:2.4.3
